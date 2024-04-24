345fc85c0fb862cb99bb1f8775fd6f83ec79ae73
Gegesilva <103137242+Gegesilva@users.noreply.github.com>
2024-04-24 16:54:49 -0300

    novo

---
+++ conexaoSQL.php
@@ -0,0 +1,11 @@
+﻿<?php
+  $serverName = 'localhost';
+  $connectionInfo = array("Database"=>"TECNOLTA", "UID"=>"sa", "PWD"=>"databit@2022", "CharacterSet"=>"UTF-8");
+  $conn = sqlsrv_connect($serverName, $connectionInfo);
+  if($conn){
+    echo "";
+  }else{
+    echo "falha na conex�o";
+    die( print_r(sqlsrv_errors(), true));
+  }
+?> 
---
+++ js/script.js
@@ -0,0 +1,53 @@
+/* INSERE PEDIDOS */
+$('#form1').submit(function(e){
+  e.preventDefault();    /*Interronpendo a atualização automatica da pagina*/ 
+
+    var d_codmoto = $('#codmoto').val();
+    var d_pedido = $('#pedido').val();
+    var d_nome = $('#nome').val();
+
+    let resultados = document.getElementById('resultados');
+
+    console.log(d_pedido, d_codmoto, d_nome);
+
+    $.ajax({
+        url: 'http://localhost:8090/phpprod/tecnolta/coletorexpedicao/inserir.php',
+        method: 'POST',
+        data: {pedido: d_pedido, codmoto: d_codmoto, nome: d_nome},
+        /* dataType: 'json' */
+    }).done(function(result){
+      $('#pedido').val('').focus();
+        console.log(result);
+        
+        resultados.innerHTML = result; 
+    });
+});
+
+
+$('#form2').submit(function(e){
+  e.preventDefault();
+
+  var d_pedido = $('#pedido').val();
+  let resultados = document.getElementById('resultados');
+
+  $.ajax({
+    url: 'http://localhost:8090/phpprod/tecnolta/coletorexpedicao/inserirTrans.php',
+    method: 'POST',
+    data: {pedido: d_pedido}
+  }).done(function(result){
+    $('#pedido').val('').focus();
+    console.log(result);
+
+    resultados.innerHTML = result;
+  })
+});
+
+
+
+  function goBack() {
+    window.history.back()
+}
+  
+function reload(){
+  Location.reload();
+}
